1d325dfbb791d2338fac38e0e6bc561b26c0ea31
Alicia Cardete Vilaplana <aliciacardetevilaplana@MacBook-Pro-de-Alicia.local>
2018-06-08 18:34:01 +0100

    Primera maquetación de las páginas en HTML5

---
+++ js/js.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    // SLIDER
+    
+    var swiper = new Swiper('.swiper-container');
+  
+
+  })
---
+++ php/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sambori ONG</title>
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+</head>
+
+<body>
+    <main>
+
+        <!-- HEADER / MENU -->
+        <header>
+        <nav>
+            <ul class="menu">
+               <li><a href="#">Sobre Sambori</a>
+               <ul class="sub-menu">
+               <li><a href="#">Nuestra historia</a></li>
+               <li><a href="#">Nuestras actividades</a></li>
+               <li><a href="#">Nuestros valores</a></li>
+               </ul>
+               </li>
+              <li><a  href="#">Quiénes somos</a></li>
+              <li><a  href="#">Voluntariado</a></li>
+              <li><a  href="#"><img class="menu__logo"
+                src="/assets/img/Iconos-y-logos/isotipo-negro.png" alt=""></a></li>
+              <li><a  href="#">Colabora</a></li>
+              <li class="menu-ultimo"><a  href="#">Contacto</a></li>
+
+            <li><a  href="#"><img class="rrss"
+                src="/assets/img/Iconos-y-logos/instragam-negro.svg" alt=""></a></li>
+            <li><a  href="#"><img class="rrss"
+            src="/assets/img/Iconos-y-logos/facebook-negro.svg" alt=""></a></li>
+            <li><a  href="#"><img class="rrss"
+            src="/assets/img/Iconos-y-logos/twitter-negro.svg" alt=""></a></li>
+            </ul>
+        </nav>
+        </header>
